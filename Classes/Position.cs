8ef69913ca31fea9c3f375a18710a7d14370b74f
Håvard Skordal Thorsen <hstst2006@gmail.com>
2023-03-15 16:12:55 +0100

    Added Position class

---
+++ Classes/Position.cs
@@ -0,0 +1,16 @@
+public class Position 
+{
+    public uint Room { get; set; } = 0;
+    public int X { get; set; } = 0;
+    public int Y { get; set; } = 0;
+
+    public Position() {}
+    public Position(uint room) 
+    { 
+        this.Room = room; 
+    }
+    public Position(uint room, int x, int y)
+    {
+        this.Room = room; this.X = X; this.Y = y;
+    }
+}
