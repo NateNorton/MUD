7ae78789895ac89696eed909f6f8fd9304942a6f
Håvard Skordal Thorsen <hstst2006@gmail.com>
2023-03-15 16:27:25 +0100

    Added interface for Describable entities

---
+++ Interfaces/IDescription.cs
@@ -0,0 +1,11 @@
+// An interface for generating and presenting descriptions
+
+interface IDescription 
+{
+    // Generate a description of the entity
+    // TODO: Is this needed?
+    void GenerateDescription();
+    // Provide/Output the description of the entity
+    void Describe();
+
+}
